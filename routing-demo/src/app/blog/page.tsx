10058898c12719ad2a31a261144862cc967dfb2c
ashishj12 <jangidashish38@gmail.com>
2025-06-09 13:10:49 +0530

    basic and nested routing in next.js

---
+++ routing-demo/src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return <h1>Blog Page</h1>;
+}
+
+// in next js to handle nesting routing we use nested folder structure to handle nested routing
+
+// e.g ->Folder(Blog) - for nested -> use nested (first) (second)
